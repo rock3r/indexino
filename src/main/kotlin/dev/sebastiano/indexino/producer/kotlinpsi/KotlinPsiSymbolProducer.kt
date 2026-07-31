0fbb9b470465a005b2113b6bd8dfbcc58c5beb34
Sebastiano Poggi <poggos@gmail.com>
2026-07-31 13:18:03 +0200

    Allow Kotlin producer origin selection

--- src/main/kotlin/dev/sebastiano/indexino/producer/kotlinpsi/KotlinPsiSymbolProducer.kt
+++ src/main/kotlin/dev/sebastiano/indexino/producer/kotlinpsi/KotlinPsiSymbolProducer.kt
@@ -37,7 +37,7 @@ import org.jetbrains.kotlin.psi.KtSuperTypeCallEntry
 import org.jetbrains.kotlin.psi.KtThisExpression
 import org.jetbrains.kotlin.psi.KtUnaryExpression
 
-@Suppress("TooManyFunctions")
+@Suppress("TooManyFunctions", "LargeClass")
 internal class KotlinPsiSymbolProducer : IndexProducer {
     override val id: String = "kotlin-psi-symbols"
     override val namespace: String = "sym"
